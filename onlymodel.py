661e5152931750b73d1d9331bb73b0bbedbed4d6
Nikita Yuriev <115225560+Serfetto@users.noreply.github.com>
2024-09-07 17:51:22 +0300

    Add files via upload

---
+++ onlymodel.py
@@ -0,0 +1,43 @@
+import os.path
+from PIL import ImageGrab, Image
+from ultralytics import YOLO
+import torch
+
+
+def screen_monitor():
+    image = ImageGrab.grab()
+    return image
+
+
+def connecting_model(path_to_model):
+    global model, load_model
+
+    model = YOLO(path_to_model, task='classify')
+
+    torch.cuda.set_device(0)
+
+    if torch.cuda.is_available():
+        device = "cuda"
+    else:
+        device = "cpu"
+
+    model.to(device)
+
+
+def classify_nsfw():
+
+    connecting_model(os.path.normpath("C:\\Users\\Serfetto\\Downloads\\last (3).pt"))
+
+    while True:
+        image = screen_monitor()
+
+        output = model.predict(image, verbose=False)[0]
+
+        probs = torch.sort(output.probs.data)
+
+        out = {output.names[idx]: f"{prob:.3f}" for idx, prob in zip(probs.indices.tolist(), probs.values.tolist())}
+
+        print(f"{out}")
+
+
+classify_nsfw()
